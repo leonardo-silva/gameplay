4fa251262109c5b1100a2eedcad69ffc4e47aaf4
Leonardo Silva <leonardosilvaif@hotmail.com>
2021-06-24 08:16:25 -0300

    Adjusting the checkbox for the scroll list elements inside Home screen - class 03

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -10,16 +10,18 @@ import { styles } from "./styles";
 type Props = RectButtonProps & {
     title: string;
     icon: React.FC<SvgProps>;
+    hasCheckBox: boolean;
     checked?: boolean;
 }
 
 export function Category({
     title,
     icon: Icon,
+    hasCheckBox = false,
     checked = false,
     ...rest
 } : Props) {
-    const { secondary50, secondary70 } = theme.colors;
+    const { secondary40, secondary50, secondary70, secondary75 } = theme.colors;
 
     return (
         <RectButton {...rest}>
@@ -27,15 +29,21 @@ export function Category({
                 style={styles.container}
                 colors={[secondary50, secondary70]}
             >
-                <View style={[styles.content, {opacity: checked ? 1 : 0.4 }]}>
-                    <View style={checked ? styles.checked : styles.check}/>
+                <LinearGradient 
+                    style={[styles.content, {opacity: checked ? 1 : 0.5 }]}
+                    colors={[checked ? secondary75 : secondary50, secondary40 ]}
+                >
+                    {   // Only shows the checkbox if hasCheckBox=true
+                        hasCheckBox &&
+                        <View style={checked ? styles.checked : styles.check}/>
+                    }    
 
                     <Icon width={48} height={48}/>
 
                     <Text style={styles.title}>
                         { title }
                     </Text>
-                </View>
+                </LinearGradient>
 
             </LinearGradient>    
             
